1fe7be687b694e458f4086c20d7c9dcfe0409124
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-08-09 16:28:24 -0400

    Add member table and seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_09_194105) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_09_201037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "groups", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "members", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_members_on_group_id"
+    t.index ["user_id"], name: "index_members_on_user_id"
+  end
+
   create_table "resources", force: :cascade do |t|
-    t.text "url"
-    t.string "type"
-    t.string "title"
-    t.string "description"
+    t.text "url", null: false
+    t.string "type", null: false
+    t.string "title", null: false
+    t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "members", "groups"
+  add_foreign_key "members", "users"
 end
